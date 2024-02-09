fe37054eb7a8f115a7345af82b8533c96adbcdc3
Keryn Gabriela Artavia Miranda <kartaviamiranda@gmail.com>
2024-02-09 01:19:39 -0600

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 //
 // Created by Maikol Guzman on 22/11/23.
 //
-#include <iostream>
-#include <cstdlib>
 
 #include "FileManagerBin.h"
-
 
 int main() {
         // Clear console screen for Windows or Linux
@@ -14,62 +11,64 @@ int main() {
     #else
         system("clear");
     #endif
-    std::cout << "Archivos binarios" << std::endl;
+
+        cout << endl;
+        cout << "BINARY FILES EXAMPLE" << endl << endl;
 
     try {
-        srand (time(NULL));
+        srand(time(NULL));
 
         Student student1;
         student1.setName("Ana");
         student1.setID("100");
         student1.setCourse("Course 1");
-        student1.setNoteCourse(0);
+        student1.setGrade(70.0);
 
         Student student2;
         student2.setName("Bob");
         student2.setID("200");
         student2.setCourse("Course 2");
-        student2.setNoteCourse(0);
+        student2.setGrade(80.0);
 
         Student student3;
         student3.setName("Carlos");
         student3.setID("300");
         student3.setCourse("Course 2");
-        student3.setNoteCourse(0);
+        student3.setGrade(90.0);
 
         // Manager SRP
         FileManagerBin fileManager;
 
-        // This function will saveStudent the information of the
-        // Student in a file with the name studentsCourse1.dat
+        // This function will save the information of the Student in a file with the name studentsCourse1.dat
         fileManager.saveStudent(student1, "studentsCourse1.dat");
 
-        // This function read a binary file
+        // This function reads a binary file
         Student studentData = fileManager.readDataStudent("studentsCourse1.dat");
 
-        cout << "[FILE BINARY READ]" << endl;
+        cout << "[FILE BINARY READ]" << endl << endl;
         cout << studentData << endl;
 
-        vector<Student> studentsList;
+        std::vector<Student> studentsList;
         studentsList.push_back(student1);
         studentsList.push_back(student2);
         studentsList.push_back(student3);
 
-        // This function will saveStudent the information of the
-        // Student in a file with the name studentsCourse.dat
+        // This function will save the information of the Student in a file with the name studentsCourse2.dat
         fileManager.saveStudentsList(studentsList, "studentsCourse2.dat");
 
-        // This function read a binary file
-        vector<Student> studentsListData = fileManager.readDataStudentList("studentsCourse2.dat");
+        // This function reads a binary file
+        vector<Student> studentsListData = fileManager.readDataStudentsList("studentsCourse2.dat");
 
-        cout << "[FILE BINARY READ WITH VECTOR]" << endl;
-        for (auto &student : studentsListData) {
-            cout << student << endl;
+        cout << endl;
+        cout << "[FILE BINARY READ WITH VECTOR]" << endl << endl;
+        for (const auto &student : studentsListData) {
+            cout << student << std::endl;
         }
-    } catch (exception& ex) {
+    } catch (std::exception& ex) {
         cerr << ex.what() << '\n' << std::endl;
     }
 
-    std::cout << std::endl;
+    cout << endl;
+
     return 0;
 }
